d550a27819f8c34a7cea4d12d4394a7734ebf127
watanabemasao <104755064+watanabemasao@users.noreply.github.com>
2022-06-21 15:40:56 +0900

    各種リクエストパラメータの練習

--- src/main/java/com/example/mappingtest/controller/GetControllerTest.java
+++ src/main/java/com/example/mappingtest/controller/GetControllerTest.java
@@ -1,15 +1,45 @@
 package com.example.mappingtest.controller;
 
+import com.example.mappingtest.form.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GetControllerTest {
     @GetMapping("names")
     public List<String> getNames() {
         return List.of("watanabe", "masao");
+        //GET http://localhost:8080/names : Body [
+        //                                        "watanabe",
+        //                                        "masao"
+        //                                       ]
     }
 
+    @GetMapping("/names/{name}")
+    public String getNames(@PathVariable String name) {
+        return String.format("name %s", name);
+        //GET http://localhost:8080/names/watanabe : Body name watanabe
+    }
+
+    @GetMapping("/get-name")
+    public String getName(@RequestParam String name) {
+        return String.format("name %s", name);
+        //GET http://localhost:8080/get-name?name=watanabe : Body name watanabe
+    }
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        // GET http://localhost:8080/greeting             Body : "id": 1, (counterにより表示回数に応じて増加)
+        //    　　　　　　　　　　　　　　　　　　　                 　 "content": "Hello, world!"(defaultValueの値)
+        // GET http://localhost:8080/greeting?name=User   Body : "id": 2, (counterにより表示回数に応じて増加)
+        //                                                       "content": "Hello, User"(name=ここの値でcontentが変化)
+    }
 }
--- src/main/java/com/example/mappingtest/controller/PatchControllerTest.java
+++ src/main/java/com/example/mappingtest/controller/PatchControllerTest.java
@@ -3,10 +3,7 @@ package com.example.mappingtest.controller;
 import com.example.mappingtest.form.UpdateForm;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -15,5 +12,10 @@ public class PatchControllerTest {
     @PatchMapping("/names/{id}")
     public ResponseEntity<Map<String, String>> update(@PathVariable("id")int id, @Validated @RequestBody UpdateForm form) {
         return ResponseEntity.ok(Map.of("message", "name successfully updated"));
+        // PATCH http://localhost:8080/names/1  Body : "message": "name successfully deleted"
+        // JSONのBodyの設定
+        // {
+        //  "name" :"watanabe",
+        // }
     }
 }
